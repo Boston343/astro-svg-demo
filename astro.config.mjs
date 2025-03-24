96be6e9932bd6e623aade3fc78801da9122db522
Boston343 <branden.austin1@gmail.com>
2025-03-24 08:36:50 -0400

    minimal repo demo

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import playformCompress from "@playform/compress";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [playformCompress()],
+  // enable importing SVGs
+  experimental: {
+    svg: {
+      mode: "sprite",
+    },
+  },
+});
